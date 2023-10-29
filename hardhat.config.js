0f7f1d3294e5a5fba3fce2c17ca3b0ffb059b013
cameronmcewan <cm17521@bristol.ac.uk>
2023-10-29 09:02:35 +0000

    update network config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,7 @@
 require('@nomiclabs/hardhat-waffle');
 require('hardhat-deploy');
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
 
 const fs = require('fs');
 let credentials = require('./credentials.example.json');
@@ -8,14 +10,33 @@ if (fs.existsSync('./credentials.json')) {
 }
 
 module.exports = {
-  networks: { ...credentials.networks.mainnets, ...credentials.networks.testnets },
+  networks: {
+    hardhat: {
+      chainId: 1337, // This is the default chainId for the local Hardhat Network
+      // You can add other configurations here, like:
+      gas: 12000000,
+      loggingEnabled: true,
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+      chainId: 11155111,
+    },
+    goerli: {
+      url: process.env.GOERLI_RPC_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+      chainId: 5,
+    },
+    // ... other networks
+  },
   solidity: {
-    version: '0.8.9',
+    version: "0.8.9",
     settings: {
       optimizer: {
         enabled: true,
         runs: 200,
       },
-    },
+    }
   },
+  // ... other Hardhat configurations
 };
